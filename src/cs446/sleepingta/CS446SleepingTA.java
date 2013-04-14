8a672e047cce6615da3c99c29e688e137272fd09
Brandon Jones <brandon@ubuntu.(none)>
2013-04-13 21:21:07 -0400

    Initial commit.

---
+++ src/cs446/sleepingta/CS446SleepingTA.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs446.sleepingta;
+
+/**
+ *
+ * @author brandon
+ */
+public class CS446SleepingTA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
